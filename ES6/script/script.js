e2bab99d634db04c86f7b9ae0d2df2487811f4b4
Cluqie <zaytsevmiron@gmail.com>
2019-04-06 15:30:34 +0300

    ES6 version

---
+++ ES6/script/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const GOODS = [
+  {title : 'Кусь-кусь', price : '900 ₽', img : 'Banner-1.png'},
+  {title : 'Хищник вышел на охоту', price : '1200 ₽', img : 'Banner-2.png'},
+  {title : 'Тело, что тупо болит', price : '900 ₽', img : 'Banner-3.png'},
+  {title : 'Череп Пашка', price : '900 ₽', img : 'Banner-4.png'},
+  {title : 'Все одно', price : '900 ₽', img : 'Banner-5.png'},
+  {title : 'Щит Щит Щит', price : '900 ₽', img : 'Banner-6.png'},
+  {title : 'И на луне приятно', price : '900 ₽', img : 'Banner-7.png'},
+  {title : 'Я иду за тобой', price : '1200 ₽', img : 'Banner-8.png'},
+  {title : '8 жизней', price : '900 ₽', img : 'Banner-9.png'},
+];
+
+const renderGoodsItem = (title, price, img) => `<div class="goods-item">
+                                                  <img src="images/${img}" alt="photo">
+                                                  <h3>${title}</h3>
+                                                  <p>${price}</p>
+                                                </div>`;
+//парсинг объекта в фрагмент верстки
+
+
+const renderGoodsList = list => {
+  // прием массива товаров
+  // передача его в RGI
+  // добавление элемента в DOM
+  const goodsList = list.map(item => renderGoodsItem (item.title,item.price, item.img)); // не знаю как записать в ES5 формате
+  document.querySelector ('.goods-list').innerHTML = goodsList.join('');
+
+};
+
+renderGoodsList (GOODS);
