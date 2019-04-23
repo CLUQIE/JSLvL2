31f2cfdfdd73534362fcc708feae37dbc548298b
Cluqie <zaytsevmiron@gmail.com>
2019-04-23 11:54:53 +0300

    homework 5

---
+++ 5 домашка/myShopVue/script.js
@@ -0,0 +1,18 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    products: [
+      {title: 'Футболка 1', img: 'http://placehold.it/360x430', price: 99, quantity: 0},
+      {title: 'Футболка 2', img: 'http://placehold.it/360x430', price: 199, quantity: 0},
+      {title: 'Футболка 3', img: 'http://placehold.it/360x430', price: 199, quantity: 0},
+      {title: 'Футболка 4', img: 'http://placehold.it/360x430', price: 149, quantity: 0},
+      {title: 'Футболка 5', img: 'http://placehold.it/360x430', price: 89, quantity: 0},
+      {title: 'Футболка 6', img: 'http://placehold.it/360x430', price: 89, quantity: 0},
+      {title: 'Футболка 7', img: 'http://placehold.it/360x430', price: 89, quantity: 0},
+      {title: 'Футболка 8', img: 'http://placehold.it/360x430', price: 89, quantity: 0},
+      {title: 'Футболка 9', img: 'http://placehold.it/360x430', price: 89, quantity: 0}]
+  },
+  methods:{
+
+  }
+})
